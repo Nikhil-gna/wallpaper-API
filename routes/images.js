a6f1ca5491433ed0316bec403e3e3bdd3f9db021
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2024-02-21 20:18:04 +0530

    Image upload 10MB limit set

--- routes/images.js
+++ routes/images.js
@@ -1,37 +1,47 @@
-const express = require('express')
+// Import necessary modules
+const express = require('express');
 const router = express.Router();
 const cloudinary = require('cloudinary').v2;
-require('dotenv').config();
+const dotenv = require('dotenv');
 const connectDB = require('../db/connect');
 const Image = require('../models/image');
 
+dotenv.config();
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.CLOUD_API_KEY, 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
-  });
+});
 
-const {getAllImages,getAllImagesTest} = require("../controllers/images");
-const { create } = require('../models/image');
+const { getAllImages, getAllImagesTest } = require("../controllers/images");
 
-router.get("/upload",(req,res)=>{
+// Route to render the upload form
+router.get("/upload", (req, res) => {
     res.render('upload');
 });
 
+// Routes for different image operations
 router.route("/").get(getAllImages);
 router.route("/upload").get(getAllImages);
 router.route("/testing").get(getAllImagesTest);
 
-
+// Route to handle image upload
 router.post('/upload', async (req, res) => {
     const file = req.files.photo;
-    console.log(file.tempFilePath);
+
+    // Check if file size exceeds 10MB
+    if (file.size > 10 * 1024 * 1024) {
+        // return res.render('alert', { message: 'File size exceeds 10MB limit.' });
+        return res.send('<script>alert("File size exceeds 10MB limit."); window.history.back();</script>');
+     
+    }
+
     cloudinary.uploader.upload(file.tempFilePath, async (err, result) => {
         if (err) {
             console.log(err);
         }
-        // res.json(result);
+
         try {
             await connectDB(process.env.MONGODB_URI);
             await Image.create({
@@ -49,7 +59,6 @@ router.post('/upload', async (req, res) => {
             console.log(error);
         }
     });
-
 });
 
-module.exports =router;
+module.exports = router;
