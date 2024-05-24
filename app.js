707b10bd4d598e9d74671e88234a874b5a9264e4
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2024-05-24 18:07:26 +0530

    added vercel prod

--- app.js
+++ app.js
@@ -5,52 +5,45 @@ const path = require("path");
 const port = process.env.PORT || 3000;
 const images_routes = require("./routes/images");
 const connectDB = require("./db/connect");
-const multer = require("multer");
-const uploads = multer({ dest: "uploads/" });
 const fileUpload = require("express-fileupload");
+const { track } = require("@vercel/analytics/server");
 
-app.use(
-  fileUpload({
-    useTempFiles: true,
-  })
-);
+app.use(fileUpload({ useTempFiles: true }));
 
-//setting up multer for storing the images in uploads folder
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-
-    console.log(file);
-  },
-});
-
-const upload = multer({ storage: storage });
-
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Define routes
 app.get("/", (req, res) => {
-  // res.send('Hello World');
+  track(req, res, "Page View", { path: "/" });
+
   res.render("home");
 });
 
 app.get("/upload", (req, res) => {
+  track(req, res, "Page View", { path: "/upload" });
+
   res.render("upload");
 });
+
 app.get("/policy", (req, res) => {
+  track(req, res, "Page View", { path: "/policy" });
+
   res.render("privacyPolicy");
 });
-// app.post('/upload',upload.single('image'), (req, res) => {
-//     res.send('File uploaded');
-// });
+
 app.post("/upload", (req, res) => {
+  track(req, res, "File Uploaded", {
+    file: req.body.fileName,
+    user: req.user.id,
+  });
   res.send("File uploaded to database");
 });
 
+// Mount API routes
 app.use("/api", images_routes);
 
+// Connect to the database and start the server
 const start = async () => {
   try {
     await connectDB();
