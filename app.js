dbce801f3b9944718817a36542b0af2ca9030733
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2024-06-29 15:25:57 +0530

    added vercel web insights

--- app.js
+++ app.js
@@ -6,52 +6,48 @@ const port = process.env.PORT || 3000;
 const images_routes = require("./routes/images");
 const connectDB = require("./db/connect");
 const fileUpload = require("express-fileupload");
-const { track } = require("@vercel/analytics/server");
+const { inject } = require("@vercel/analytics");
 
+// Middleware for file upload
 app.use(fileUpload({ useTempFiles: true }));
 
+// Set view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Define routes
 app.get("/", (req, res) => {
-  track(req, res, "Page View", { path: "/" });
-
   res.render("home");
 });
 
 app.get("/upload", (req, res) => {
-  track(req, res, "Page View", { path: "/upload" });
-
   res.render("upload");
 });
 
 app.get("/policy", (req, res) => {
-  track(req, res, "Page View", { path: "/policy" });
-
   res.render("privacyPolicy");
 });
 
 app.post("/upload", (req, res) => {
-  track(req, res, "File Uploaded", {
-    file: req.body.fileName,
-    user: req.user.id,
-  });
+  // Tracking file upload event
   res.send("File uploaded to database");
 });
 
 // Mount API routes
 app.use("/api", images_routes);
 
+// Inject Vercel Analytics
+inject();
+
 // Connect to the database and start the server
 const start = async () => {
   try {
     await connectDB();
     app.listen(port, () => {
-      console.log(`Server is running on port :${port} `);
+      console.log(`Server is running on port :${port}`);
     });
   } catch (error) {
-    console.log(error);
+    console.error("Failed to start the server:", error);
   }
 };
 
